0aa91ab59434c6b9ed83e9332ce95cb7430801b6
vincentcentwise <balogun_vincent@yahoo.com>
2026-07-09 21:00:52 +0100

    done

--- dglobal/scripts/scripts.js
+++ dglobal/scripts/scripts.js
@@ -1,14 +1,59 @@
+
+
 const form = document.getElementById("trainingForm");
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", async function (e) {
 
     e.preventDefault();
 
-    const fullname =
-    document.getElementById("fullname").value;
+    const registrationID =
+        "DGF-" + Date.now();
 
-    document.getElementById("successMessage").innerHTML =
-    `Thank you ${fullname}! Your registration has been submitted successfully.`;
+    const data = {
 
-    form.reset();
+        fullname:
+            document.getElementById("fullname").value,
+
+        school:
+            document.getElementById("school").value,
+
+        referral:
+            document.getElementById("referral").value,
+
+        location:
+            document.getElementById("location").value,
+
+        whatsapp:
+            document.getElementById("whatsapp").value,
+
+        registrationID:
+            registrationID
+
+    };
+
+    try {
+
+        await fetch(
+            "https://script.google.com/macros/s/AKfycbxP5FFQZIhJMRx8-ynk2BLfJVG3khWmQPnS7Miq6ADaOfvNs9iI5ZWyDoj1k0b4aH33Zw/exec",
+            {
+
+                method: "POST",
+
+                body: JSON.stringify(data)
+
+            }
+        );
+
+        alert("Registration Successful!");
+
+        form.reset();
+
+    }
+
+    catch (error) {
+
+        alert("Submission Failed");
+
+    }
+
 });
