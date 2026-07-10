86edcdcdfebc8d26fb8fa641952d3444a680273d
vincentcentwise <balogun_vincent@yahoo.com>
2026-07-10 10:46:48 +0100

    done

--- dglobal/scripts/scripts.js
+++ dglobal/scripts/scripts.js
@@ -1,69 +1,56 @@
+const WEB_APP_URL =
+"https://script.google.com/macros/s/AKfycbwRUntiBDBMCgFBMa6VXYdErK5LXv7fNs4ePKRPPTBljsZxyMh1r2n6yKyCWNkJ5U0z4A/exec";
+
 const form = document.getElementById("trainingForm");
 
-const WEB_APP_URL =
-"https://script.google.com/macros/s/AKfycbwpcpzd0KAhkUzQMv9Wy0ODDy5K3yFGZqTzEQviyrVz9NbZzVuEpe49HcAQSkKeRygKBw/exec";
-
-form.addEventListener("submit", async function(e){
+form.addEventListener("submit", async function (e) {
 
     e.preventDefault();
 
+    const submitButton = form.querySelector("button");
+
+    submitButton.disabled = true;
+    submitButton.textContent = "Submitting...";
+
     const formData = new FormData();
 
-    formData.append(
-        "fullname",
-        document.getElementById("fullname").value
-    );
+    formData.append("fullname", document.getElementById("fullname").value);
+    formData.append("school", document.getElementById("school").value);
+    formData.append("referral", document.getElementById("referral").value);
+    formData.append("location", document.getElementById("location").value);
+    formData.append("whatsapp", document.getElementById("whatsapp").value);
+    formData.append("registrationID", "DGF-" + Date.now());
 
-    formData.append(
-        "school",
-        document.getElementById("school").value
-    );
+    try {
 
-    formData.append(
-        "referral",
-        document.getElementById("referral").value
-    );
-
-    formData.append(
-        "location",
-        document.getElementById("location").value
-    );
-
-    formData.append(
-        "whatsapp",
-        document.getElementById("whatsapp").value
-    );
-
-    formData.append(
-        "registrationID",
-        "DGF-"+Date.now()
-    );
-
-    try{
-
-        const response = await fetch(WEB_APP_URL,{
-            method:"POST",
-            body:formData
+        const response = await fetch(WEB_APP_URL, {
+            method: "POST",
+            body: formData
         });
 
-        if(response.ok){
+        const result = await response.json();
+
+        if (result.status === "success") {
 
             alert("Registration Successful!");
 
             form.reset();
 
-        }else{
+        } else {
 
-            alert("Server returned an error.");
+            alert(result.message);
 
         }
 
-    }catch(error){
+    } catch (error) {
 
         console.error(error);
 
         alert("Submission Failed");
 
     }
+
+    submitButton.disabled = false;
+    submitButton.textContent = "Submit Registration";
 
 });
